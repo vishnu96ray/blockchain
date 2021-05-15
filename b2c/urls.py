3bcf35c51ccdfde3639192a83225821f8dd92d27
vishnu ray <vishnu96.ray@gmail.com>
2021-05-15 14:05:52 +0530

    changes

--- b2c/urls.py
+++ b2c/urls.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-admin.autodiscover()
-admin.site.enable_nav_sidebar = False
-from django.conf import settings
-from django.conf.urls.static import static
 	
 urlpatterns = [
     path('', include('accounts.urls')),
