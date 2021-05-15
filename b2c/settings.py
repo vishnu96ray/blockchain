d4aec8d86f2173d6d1ab33602868bb8e48d073df
vishnu96ray <73793874+vishnu96ray@users.noreply.github.com>
2021-05-15 17:02:26 +0530

    Update settings.py

--- b2c/settings.py
+++ b2c/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'b2c.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR , 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
