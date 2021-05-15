42b4eaf793d7b6a354ec460eaace8659756b677f
vishnu ray <vishnu96.ray@gmail.com>
2021-05-16 00:44:24 +0530

    changes

--- b2c/settings.py
+++ b2c/settings.py
@@ -133,10 +133,10 @@ except ImportError:
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "collectstatic")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
