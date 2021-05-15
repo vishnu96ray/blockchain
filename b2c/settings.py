301223fc1d8302100fbddb79e387b20dfa2ec6f4
vishnu ray <vishnu96.ray@gmail.com>
2021-05-15 13:17:53 +0530

    changes

--- b2c/settings.py
+++ b2c/settings.py
@@ -42,10 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'accounts',
-    'dprocess',
     'business',
+    'dprocess',
+    
 ]
 
 MIDDLEWARE = [
