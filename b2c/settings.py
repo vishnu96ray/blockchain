1e4477f67fe9025a29ba4a19696314e9fad1432f
vishnu96ray <73793874+vishnu96ray@users.noreply.github.com>
2021-05-15 17:48:53 +0530

    Update settings.py

--- b2c/settings.py
+++ b2c/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'template'),
 
 
 
