e8d1770ad4cb21767086d84709e6fd7b5f251d40
vishnu ray <vishnu96.ray@gmail.com>
2021-05-15 11:22:36 +0530

    changes

--- b2c/settings.py
+++ b2c/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'b2c.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, "templates"],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
