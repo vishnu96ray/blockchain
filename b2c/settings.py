07067a734ffefefdce41dc29bdd70b5d23d18eec
vishnu96ray <73793874+vishnu96ray@users.noreply.github.com>
2021-05-15 14:27:05 +0530

    Update settings.py

--- b2c/settings.py
+++ b2c/settings.py
@@ -58,13 +58,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'b2c.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
